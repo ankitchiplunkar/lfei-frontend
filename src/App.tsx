293abee935b3777f009b4b6f93f9bb73ad16f4a2
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2021-04-07 23:42:14 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Redirect, Route, Switch } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
+import { Page } from './components/base/base'
+import { TopBar } from './components/TopBar'
+import { GlobalStyle } from './global/GlobalStyle'
+import { Balance } from './pages/Balance'
+import { Transactions } from './pages/Transactions'
+
+export function App() {
+  return (
+    <Page>
+      <GlobalStyle />
+      <BrowserRouter>
+        <TopBar />
+        <Switch>
+          <Route exact path="/balance" component={Balance} />
+          <Route exact path="/transactions" component={Transactions} />
+          <Redirect exact from="/" to="/balance" />
+        </Switch>
+      </BrowserRouter>
+    </Page>
+  )
+}
