dcc86cd76ab1d8cac03418864d89989a6e06db21
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2021-04-08 22:39:01 +0100

    add token balances

--- src/pages/Balance.tsx
+++ src/pages/Balance.tsx
@@ -1,20 +1,21 @@
 import { formatEther } from '@ethersproject/units'
-import { useEtherBalance, useEthers } from '@usedapp/core'
+import { useEtherBalance, useEthers, useTokenBalance } from '@usedapp/core'
 import styled from 'styled-components'
 import { Container, ContentBlock, ContentRow, MainContent, Section, SectionRow } from '../components/base/base'
 import { Button } from '../components/base/Button'
 import { DepositEth, WithdrawEth } from '../components/Notifications/Forms'
-import { TokensList } from '../components/TokensList/TokensList'
 import { Label } from '../typography/Label'
 import { TextInline } from '../typography/Text'
 import { Title } from '../typography/Title'
 
-const STAKING_CONTRACT = '0x00000000219ab540356cBB839Cbe05303d7705Fa'
-
 export function Balance() {
   const { activateBrowserWallet, deactivate, account, library } = useEthers()
   const userBalance = useEtherBalance(account)
-  const stakingBalance = useEtherBalance(STAKING_CONTRACT)
+  const feiAddress = "0x85539BBcC27Ec15C8763ceC68a99C14903420E34"
+  const usdcAddress = "0xb2cDD6A87B147e32c8e279e989101e373Ae90782"
+  const feiBalance = useTokenBalance(feiAddress, account)
+  const usdcBalance = useTokenBalance(usdcAddress, account)
+  console.log(feiBalance, usdcBalance)
 
   return (
     <MainContent>
@@ -29,12 +30,6 @@ export function Balance() {
               )}
           </SectionRow>
           <ContentBlock>
-            {stakingBalance && (
-              <ContentRow>
-                <Label>ETH2 staking contract holds:</Label> <TextInline>{formatEther(stakingBalance)}</TextInline>{' '}
-                <Label>ETH</Label>
-              </ContentRow>
-            )}
             {account && (
               <ContentRow>
                 <Label>Account:</Label> <TextInline>{account}</TextInline>
@@ -45,12 +40,20 @@ export function Balance() {
                 <Label>Ether balance:</Label> <TextInline>{formatEther(userBalance)}</TextInline> <Label>ETH</Label>
               </ContentRow>
             )}
+            {feiBalance && (
+              <ContentRow>
+                <Label>Fei balance:</Label> <TextInline>{formatEther(feiBalance)}</TextInline> <Label>Fei</Label>
+              </ContentRow>
+            )}
+            {feiBalance && (
+              <ContentRow>
+                <Label>USDC balance:</Label> <TextInline>{formatEther(feiBalance)}</TextInline> <Label>USDC</Label>
+              </ContentRow>
+            )}
           </ContentBlock>
-          <TokensContentBlock>
-            <TokensList />
-          </TokensContentBlock>
           <TableGrid>
             {account && library && <DepositEth account={account} library={library} />}
+            {account && library && <WithdrawEth account={account} library={library} />}
             {account && library && <WithdrawEth account={account} library={library} />}
             {account && library && <WithdrawEth account={account} library={library} />}
           </TableGrid>
@@ -66,6 +69,6 @@ const TokensContentBlock = styled(ContentBlock)`
 
 const TableGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   gap: 1px;
 `
