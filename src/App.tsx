1501ae2c55f57cf7402f40380014624a83aacda9
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2021-04-12 13:44:38 +0100

    deployed to website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route, Switch } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Page } from './components/base/base'
 import { TopBar } from './components/TopBar'
 import { GlobalStyle } from './global/GlobalStyle'
@@ -9,13 +9,13 @@ export function App() {
   return (
     <Page>
       <GlobalStyle />
-      <BrowserRouter>
+      <HashRouter>
         <TopBar />
         <Switch>
           <Route exact path="/lfei" component={Balance} />
           <Redirect exact from="/" to="/lfei" />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </Page>
   )
 }
